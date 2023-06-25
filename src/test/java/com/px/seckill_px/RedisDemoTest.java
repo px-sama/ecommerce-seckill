57a0928c180ce194cf997abb01bdcd33f7898616
px-sama <danielxu93@gmail.com>
2023-06-25 14:59:00 -0400

    Added feature to get and release distributed lock.

--- src/test/java/com/px/seckill_px/RedisDemoTest.java
+++ src/test/java/com/px/seckill_px/RedisDemoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.UUID;
 
 @SpringBootTest
 public class RedisDemoTest {
@@ -35,6 +36,29 @@ public class RedisDemoTest {
     @Test
     public void pushSeckillInfoToRedisTest() {
         seckillActivityService.pushSeckillInfoToRedis(19);
+    }
+
+    /**
+     * 测试高并发下获取锁的结果 */
+    @Test
+    public void  testConcurrentAddLock() {
+        for (int i = 0; i < 10; i++) {String requestId = UUID.randomUUID().toString();
+        // 打印结果 true false false false false false false false false false
+        // 只有第一个能获得 锁
+            System.out.println(redisService.tryGetDistributedLock("A", requestId,1000));
+        }
+    }
+
+    /**
+     * 测试并发下获取锁然后立刻释放锁的结果 */
+    @Test
+    public void  testConcurrent() {
+        for (int i = 0; i < 10; i++) {
+            String requestId = UUID.randomUUID().toString();
+        // 打印结果 true true true true true true true true true true
+            System.out.println(redisService.tryGetDistributedLock("A", requestId,1000));
+            redisService.releaseDistributedLock("A", requestId);
+        }
     }
 
 }
